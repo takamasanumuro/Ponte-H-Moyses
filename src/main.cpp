6f3f96e5517b2f8f9c84b68115a77356fc1a2f3b
adrianowerneck <adrianowerneck@id.uff.br>
2024-06-12 13:51:35 -0300

    Added direction rotation

--- src/main.cpp
+++ src/main.cpp
@@ -3,22 +3,37 @@
 #define outputA 8  
 #define outputB 7 
 #define potentiometerPin A0
-
+#define buttonChangeDirectionPin 6
 
 void setup() {
+    Serial.begin(9600);
     pinMode(outputPWM, OUTPUT);
     pinMode(outputA, OUTPUT);
     pinMode(outputB, OUTPUT);
     pinMode(potentiometerPin, INPUT);
-    Serial.begin(9600);
+    pinMode(buttonChangeDirectionPin, INPUT_PULLUP);
+
 }
 
 void loop() {
+
+    bool buttonState = digitalRead(buttonChangeDirectionPin);
+
+    if (buttonState == LOW) {
+        digitalWrite(outputA, LOW);
+        digitalWrite(outputB, HIGH);
+    } else {
+        digitalWrite(outputA, HIGH);
+        digitalWrite(outputB, LOW);
+    }
+
     int potValue = analogRead(potentiometerPin);
     int motorSpeed = map(potValue, 0, 1023, 0, 255);
     analogWrite(outputPWM, motorSpeed);
     digitalWrite(outputA, HIGH);
     digitalWrite(outputB, LOW);
+
+
 
     static unsigned long lastTime = 0;
     unsigned long currentTime = millis();
@@ -30,6 +45,10 @@ void loop() {
 
         Serial.print("Motor speed: ");
         Serial.println(motorSpeed);
+        
+        Serial.print("Button state: ");
+        Serial.println(buttonState);
+        
     }
 
 
